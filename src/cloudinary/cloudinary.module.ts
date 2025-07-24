d291b1be21f260ee65e8538ee68a11eca33511bc
Amani Ishimwe <amaniishimwe36@gmail.com>
2025-07-24 17:17:42 +0300

    fixed database connections

--- src/cloudinary/cloudinary.module.ts
+++ src/cloudinary/cloudinary.module.ts
@@ -8,4 +8,3 @@ import { CloudinaryService } from './cloudinary.service';
 })
 
 export class CloudinaryModule {}
-
--- src/database/database.service.ts
+++ src/database/database.service.ts
@@ -7,4 +7,8 @@ export class DatabaseService extends PrismaClient implements OnModuleInit{
         await this.$connect();
         console.log('Database Postgres connected');
     }
+    async onModuleDestroy() {
+        await this.$disconnect();
+        console.log('Database Postgres disconnected');
+    }
 }
